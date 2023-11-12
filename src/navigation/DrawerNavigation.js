12701e7ab925a26b1e081222a3d948e7a12632ea
Alisha <alishasultan0123@gmail.com>
2023-11-12 12:06:42 +0500

    modifies

---
+++ src/navigation/DrawerNavigation.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import RootClientTab from './ClientTab';
+import {colors} from '../Global/styles';
+import {Icon} from 'react-native-elements';
+
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigation() {
+
+    return(
+        <Drawer.Navigator>
+      
+         <Drawer.Screen 
+            name = "RootClientTab"
+            component={RootClientTab}
+
+            options = {{
+                title:'Client',
+                drawerIcon:({focussed,size})=>(
+                    <Icon 
+                     type = "material-community"
+                     name = 'home'
+                     color = {focussed?"#E11584":colors.buttons}
+                     size = {size}
+
+                    
+                    
+                    />
+                )
+
+            }}
+         
+         
+         />
+
+        </Drawer.Navigator>
+    )
+}
